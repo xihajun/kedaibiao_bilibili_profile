88fe5915c5969fbcd38d22e6707d04dc7252c245
xihajun <mw18386@my.bristol.ac.uk>
2024-12-09 22:10:01 +0000

    add "use client";

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState } from 'react';
 import { Search, Video, X, ExternalLink } from 'lucide-react';
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert';
